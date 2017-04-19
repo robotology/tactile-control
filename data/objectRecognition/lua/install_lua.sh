8999384f3fa325e672f941c2722ae396a26204e1
Massimo Regoli <massimo.regoli@iit.it>
2017-04-19 18:24:42 +0200

    removed unused data

--- data/objectRecognition/lua/install_lua.sh
+++
@@ -1,5 +0,0 @@
-cp ./tactile_object_recognition_main.lua $ICUBcontrib_DIR/bin/
-cp ./tactile_object_recognition_funcs.lua $ICUBcontrib_DIR/share/ICUBcontrib/contexts/tactile-object-recognition/lua/
-cp ./tactile_object_recognition_interact_fsm.lua $ICUBcontrib_DIR/share/ICUBcontrib/contexts/tactile-object-recognition/lua/
-cp ./tactile_object_recognition_root_fsm.lua $ICUBcontrib_DIR/share/ICUBcontrib/contexts/tactile-object-recognition/lua/
-
